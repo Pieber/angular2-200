fa066155d2a9ff7939663637fc51aa957ff8bdbc
Cyril Balit <cbalit@gmail.com>
2018-03-22 14:34:46 +0100

    remove person

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,6 @@ import { APP_ROUTES } from './app.routes';
 
 import { PeopleAppComponent } from './app.component';
 import { HomeComponent } from './home';
-import { PersonComponent } from './person';
 import { PeopleComponent } from './people';
 import { CardComponent } from './shared/card';
 import { AddDialogComponent } from './people/add-dialog/add-dialog.component';
@@ -52,7 +51,6 @@ import { NaPipe } from 'app/shared/na-pipe';
   declarations: [
     PeopleAppComponent,
     HomeComponent,
-    PersonComponent,
     PeopleComponent,
     CardComponent,
     AddDialogComponent,
