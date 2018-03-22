7b153221da388720bc668aa28ede2312372a6e90
Cyril Balit <cbalit@gmail.com>
2018-03-22 14:26:32 +0100

    remove person

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,6 @@ import { APP_ROUTES } from './app.routes';
 
 import { PeopleAppComponent } from './app.component';
 import { HomeComponent } from './home';
-import { PersonComponent } from './person';
 import { PeopleComponent } from './people';
 import { CardComponent } from './shared/card';
 import { AddDialogComponent } from './people/add-dialog/add-dialog.component';
@@ -50,7 +49,6 @@ import { UpdateComponent } from './update/update.component';
   declarations: [
     PeopleAppComponent,
     HomeComponent,
-    PersonComponent,
     PeopleComponent,
     CardComponent,
     AddDialogComponent,
