1468baf05dba5feb3b30f128682b400782f72068
Pieber <pierrebernard.lr@gmail.com>
2018-05-21 11:04:32 +0200

    Exercise 4 - Personal Version

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PEOPLE} from '../_static/people';
 
 @Component({
     selector: 'sfeir-home',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['home.component.css']
 })
 export class HomeComponent implements OnInit {
-    name:string;
+    private person: any;
     
     constructor() {
-        this.name = 'Angular 2';
+        this.person = PEOPLE[0];
     }
 
     /**
